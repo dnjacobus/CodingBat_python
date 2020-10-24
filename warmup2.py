881be0d9d0c722a4a3bbce332c06515c96c5a3f0
David Jacobus <nilesdj6@gmail.com>
2020-10-24 19:28:18 +0200

    Warmup2 Completion
    Completed last function of Warmup2.

--- warmup2.py
+++ warmup2.py
@@ -115,3 +115,16 @@ def array123(nums):
         if nums[i:i+3] == match_list:
         return True
     return False
+
+def string_match(a, b):
+    '''
+    Given 2 strings, a and b, return the number of the positions where they contain the same length 2 substring. 
+    So "xxcaazz" and "xxbaaz" yields 3, since the "xx", "aa", 
+    and "az" substrings appear in the same place in both strings.
+    '''
+    short = min(a,b)
+    count = 0
+    for i in range(len(short)-1):
+        if a[i:i+2] == b[i:i+2]:
+            count += 1
+    return count
